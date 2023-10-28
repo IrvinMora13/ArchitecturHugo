10fff0900e222a482d177e49dbbda41dac267c81
Irvin Morales <a343423@gmail.com>
2023-10-28 17:51:05 -0600

    Hero

--- PageArchitecture/src/App.jsx
+++ PageArchitecture/src/App.jsx
@@ -1,10 +1,14 @@
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
+import "./App.css"
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
+      <div>
+        <div className="whiteGradient"/>
+        <Header />
+        <Hero />
+      </div>
 
     </div>
   )
--- PageArchitecture/src/components/hero/Hero.jsx
+++ PageArchitecture/src/components/hero/Hero.jsx
@@ -15,6 +15,18 @@ const Hero = () => {
                             <br />¿Estás listo?
                         </h1>
                     </div>
+                    <div className='heroDes flexColStart'>
+<span className='secondaryText'>Somos una empresa mexicana dedicada a la construccion de proyectos personales como comerciales.</span>
+<span className='secondaryText'>Incluimos el total desarrollo de su proyecto desde diseño hasta construccion</span>
+                    </div>
+                    <div className='flexStart buttonNav'>
+                        <button className="button">Proyectos</button>
+                        <button className="button">Contacto</button>
+                        <button className="button">Acerca </button>
+                    </div>
+                    <div className='flexColStart stats'>
+                        /*Pendientes minute 41*/
+                    </div>
                 </div>
                 <div className="flexCenter heroRight">
                     <div className="imgContainer">
