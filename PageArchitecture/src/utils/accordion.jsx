e998b4525c6a3f63862b3732ea3b144034e89ac6
Irvin Morales <a343423@gmail.com>
2023-11-07 10:05:30 -0600

    Valor

--- PageArchitecture/src/utils/accordion.jsx
+++ PageArchitecture/src/utils/accordion.jsx
@@ -3,18 +3,18 @@ import { MdCancel, MdAnalytics } from "react-icons/md";
 const data = [
     {
         icon:<HiShieldCheck/>,
-        heading: "Best interest",
-        detail:"Best Integreation of the world "
+        heading: "Experiencia y profesionalismo",
+        detail:"Nuestra empresa constructora se enorgullece de su amplia experiencia en la industria.Contamos con un equipo de profesionales altamente capacitados y con una larga trayectoria en la construcción. Nuestra experiencia nos permite abordar proyectos de manera eficiente, resolver desafíos de construcción de manera efectiva y garantizar que cada proyecto se complete con los más altos estándares de calidad y seguridad."
     },
     {
         icon:<MdCancel/>,
-        heading: "Unstable prices",
-        detail:"Best Integreation of the world "
+        heading: "Calidad en la construcción",
+        detail:"La calidad en la construcción es un pilar fundamental de nuestra empresa. Nos comprometemos a utilizar materiales de primera calidad y técnicas de construcción avanzadas en cada proyecto que emprendemos. Nuestro enfoque en la calidad se traduce en estructuras sólidas y duraderas, que resisten el paso del tiempo y cumplen con las expectativas de nuestros clientes. "
     },
     {
         icon:<MdAnalytics/>,
-        heading: "Best Price",
-        detail:"Best Integreation of the world "
+        heading: "Precio competitivo",
+        detail:"Entendemos la importancia de ofrecer un precio competitivo en el mercado de la construcción. Nuestra empresa se esfuerza por mantener tarifas asequibles sin comprometer la calidad del trabajo. Trabajamos de manera eficiente, optimizando costos sin sacrificar la integridad de nuestros proyectos. Ofrecer precios competitivos nos permite brindar a nuestros clientes una excelente relación calidad-precio y la tranquilidad de saber que están obteniendo un valor excepcional por su inversión."
     },
 ]
 export default data;
