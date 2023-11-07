42f9b29b1b1467424965ae1fa1c2915f21f9744d
Irvin Morales <a343423@gmail.com>
2023-11-06 21:02:33 -0600

    Value

--- PageArchitecture/src/components/value/Value.jsx
+++ PageArchitecture/src/components/value/Value.jsx
@@ -7,11 +7,59 @@ import {
     AccordionItemPanel,
     AccordionItemState
 } from 'react-accessible-accordion';
-
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import Img from "../../assets/ImgFondoInicio.jpg"
+import './value.css';
+import data from "../../utils/accordion"
 
 const Value = () => {
     return (
-        <div>Value</div>
+       <section className="vWrapper">
+        <div className="paddings innerWidth flexCenter vContainer">
+            {/*Left Images*/}
+            <div className="vLeft">
+                <div className="imageContainer">
+                    <img src={Img} alt="" />
+                </div>
+            </div>
+            {/*Rigt Information*/}
+            <div className="flexColStart vRight">
+            <span className="orangeText">Nuestro Valor</span>
+                <span className="primaryText">Que te podemos ofrecer</span>
+                <span className="secondaryText">Nosotros siempre estamos listos para ayudar a cualquier proyecto.
+                    <br/>
+                    En cualquier lugar ofreciendo los mejores servicios
+                    <br/>
+                    Desde el diseño hasta la finalizacion del proyecto
+                </span>
+                <Accordion className="accordion" allowMultipleExpanded={false} preExpanded={[0]}>
+                    {
+                        data.map((item, i) => {
+                            return (
+                                <AccordionItem className="accordionItem" key={i} uuid={i}>
+                                    <AccordionItemHeading>
+                                        <AccordionItemButton>
+                                            <div className="flexCenter icon">
+                                                {item.icon}
+                                            </div>
+                                            <span className="primaryText">
+                                                {item.heading}
+                                            </span>
+                                            <div className="flexCenter icon">
+                                                <MdOutlineArrowDropDown size={20}/>
+                                            </div>
+                                        </AccordionItemButton>
+                                    </AccordionItemHeading>
+                                </AccordionItem>
+                            )
+                        })
+                    }
+                
+                </Accordion>
+            </div>
+        </div>
+       </section>
     )
 }
 export default Value;
---
+++ PageArchitecture/src/utils/accordion.jsx
@@ -0,0 +1,20 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+    {
+        icon:<HiShieldCheck/>,
+        heading: "Best interest",
+        detail:"Best Integreation of the world "
+    },
+    {
+        icon:<MdCancel/>,
+        heading: "Unstable prices",
+        detail:"Best Integreation of the world "
+    },
+    {
+        icon:<MdAnalytics/>,
+        heading: "Best Price",
+        detail:"Best Integreation of the world "
+    },
+]
+export default data;
