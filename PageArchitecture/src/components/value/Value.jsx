9cc478206933914789f52b40b9a67a2fbada94fd
Irvin Morales <a343423@gmail.com>
2023-11-08 23:08:08 -0600

    Update Value.jsx

--- PageArchitecture/src/components/value/Value.jsx
+++ PageArchitecture/src/components/value/Value.jsx
@@ -14,66 +14,62 @@ import './value.css';
 import data from "../../utils/accordion"
 
 const Value = () => {
+    const [accordionItems, setAccordionItems] = useState(data.map(() => false)); // Initialize all items as collapsed
+
+    const handleAccordionItemClick = (index) => {
+        const newAccordionItems = [...accordionItems];
+        newAccordionItems[index] = !newAccordionItems[index];
+        setAccordionItems(newAccordionItems);
+    }
+
     return (
-       <section className="vWrapper">
-        <div className="paddings innerWidth flexCenter vContainer">
-            {/*Left Images*/}
-            <div className="vLeft">
-                <div className="imageContainer">
-                    <img src={Img} alt="" />
+        <section className="vWrapper">
+            <div className="paddings innerWidth flexCenter vContainer">
+                {/* Left Images */}
+                <div className="vLeft">
+                    <div className="imageContainer">
+                        <img src={Img} alt="" />
+                    </div>
+                </div>
+                {/* Right Information */}
+                <div className="flexColStart vRight">
+                    <span className="orangeText">Nuestro Valor</span>
+                    <span className="primaryText">Que te podemos ofrecer</span>
+                    <span className="secondaryText">Nosotros siempre estamos listos para ayudar a cualquier proyecto.
+                        <br />
+                        En cualquier lugar ofreciendo los mejores servicios
+                        <br />
+                        Desde el diseño hasta la finalización del proyecto
+                    </span>
+                    <Accordion className="accordion" allowMultipleExpanded={false} preExpanded={[0]}>
+                        {data.map((item, i) => (
+                            <AccordionItem
+                                className={`accordionItem ${accordionItems[i] ? "expanded" : "collapsed"}`}
+                                key={i}
+                                uuid={i}
+                            >
+                                <AccordionItemHeading>
+                                    <AccordionItemButton
+                                        className="accordionButton flexCenter"
+                                        onClick={() => handleAccordionItemClick(i)}
+                                    >
+                                        <div className="flexCenter icon">{item.icon}</div>
+                                        <span className="primaryText">{item.heading}</span>
+                                        <div className="flexCenter icon">
+                                            <MdOutlineArrowDropDown size={20} />
+                                        </div>
+                                    </AccordionItemButton>
+                                </AccordionItemHeading>
+                                <AccordionItemPanel>
+                                    <p className="secondaryText">{item.detail}</p>
+                                </AccordionItemPanel>
+                            </AccordionItem>
+                        ))}
+                    </Accordion>
                 </div>
             </div>
-            {/*Rigt Information*/}
-            <div className="flexColStart vRight">
-            <span className="orangeText">Nuestro Valor</span>
-                <span className="primaryText">Que te podemos ofrecer</span>
-                <span className="secondaryText">Nosotros siempre estamos listos para ayudar a cualquier proyecto.
-                    <br/>
-                    En cualquier lugar ofreciendo los mejores servicios
-                    <br/>
-                    Desde el diseño hasta la finalizacion del proyecto
-                </span>
-                <Accordion className="accordion" allowMultipleExpanded={false} preExpanded={[0]}>
-                    {
-                        data.map((item, i) => {
-                                const [className, setClassName] = useState(null)
-                            return (
-                                <AccordionItem className={`accordionItem ${className}`} key={i} uuid={i}>
-                                    <AccordionItemHeading>
-                                        <AccordionItemButton className="accordionButton flexCenter">
-
-                                            <AccordionItemState>
-                                                {({expanded}) => 
-                                                 expanded 
-                                                    ? setClassName("expanded")
-                                                    : setClassName("collapsed")}
-                                            </AccordionItemState>
-
-                                            <div className="flexCenter icon">
-                                                {item.icon}
-                                            </div>
-                                            <span className="primaryText">
-                                                {item.heading}
-                                            </span>
-                                            <div className="flexCenter icon">
-                                                <MdOutlineArrowDropDown size={20}/>
-                                            </div>
-                                        </AccordionItemButton>
-                                    </AccordionItemHeading>
-                                    <AccordionItemPanel>
-                                        <p className="secondaryText">
-                                            {item.detail}
-                                        </p>
-                                    </AccordionItemPanel>
-                                </AccordionItem>
-                            )
-                        })
-                    }
-                
-                </Accordion>
-            </div>
-        </div>
-       </section>
+        </section>
     )
 }
+
 export default Value;
